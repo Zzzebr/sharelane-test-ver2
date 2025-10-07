4d825534ea5fcafbeab02439fc3482d311c4b70b
Kluchnik-N <ilya.vasilevv@bk.ru>
2025-10-07 09:30:22 +0300

    Extra empty lines removed

--- src/test/java/RegTest.java
+++ src/test/java/RegTest.java
@@ -1,15 +1,12 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.Assert;
-
 import java.time.Duration;
 
 
@@ -20,7 +17,6 @@ public class RegTest
     @BeforeMethod
     public void Start()
     {
-
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\79291\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://sharelane.com/cgi-bin/register.py");
@@ -30,35 +26,29 @@ public class RegTest
     public void checkPositiveLoginTest() {
         driver.findElement(By.name("zip_code")).sendKeys("12345");
         driver.findElement(By.cssSelector("[value='Continue']")).click();
-
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("first_name")));
-
         driver.findElement(By.name("first_name")).sendKeys("username");
         driver.findElement(By.name("last_name")).sendKeys("lastname");
         String uniqueEmail = "test@example.com";
         driver.findElement(By.name("email")).sendKeys(uniqueEmail);
-        String password = "password1";
+        String password = "Nhhfgk654";
         driver.findElement(By.name("password1")).sendKeys(password);
         driver.findElement(By.name("password2")).sendKeys(password);
         driver.findElement(By.cssSelector("[value='Register']")).click();
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/center/table/tbody/tr[6]/td/table/tbody/tr[4]/td/table/tbody/tr[1]/td[2]/b")));
-
         String email;
         email = driver.findElement(By.xpath("/html/body/center/table/tbody/tr[6]/td/table/tbody/tr[4]/td/table/tbody/tr[1]/td[2]/b")).getText();
 
         driver.findElement(By.xpath("/html/body/center/table/tbody/tr[1]/td/table/tbody/tr/td[1]/a")).click();
-
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[value='Login']")));
-
         driver.findElement(By.name("email")).sendKeys(email);
         driver.findElement(By.name("password")).sendKeys("1111");
         driver.findElement(By.cssSelector("[value='Login']")).click();
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/center/table/tbody/tr[1]/td/table/tbody/tr/td[2]/span")));
-
         boolean elementOnPage;
         if (driver.findElement(By.xpath("/html/body/center/table/tbody/tr[1]/td/table/tbody/tr/td[2]/span")).isDisplayed())
             elementOnPage = true;
@@ -74,7 +64,6 @@ public class RegTest
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("first_name")));
-
         driver.findElement(By.name("first_name")).sendKeys("username");
         driver.findElement(By.name("last_name")).sendKeys("lastname");
         String uniqueEmail = "test@example.com";
@@ -85,25 +74,19 @@ public class RegTest
         driver.findElement(By.cssSelector("[value='Register']")).click();
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/center/table/tbody/tr[6]/td/table/tbody/tr[4]/td/table/tbody/tr[1]/td[2]/b")));
-
         String email;
         email = driver.findElement(By.xpath("/html/body/center/table/tbody/tr[6]/td/table/tbody/tr[4]/td/table/tbody/tr[1]/td[2]/b")).getText();
-
         driver.findElement(By.xpath("/html/body/center/table/tbody/tr[1]/td/table/tbody/tr/td[1]/a")).click();
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[value='Login']")));
-
         driver.findElement(By.name("email")).sendKeys(email);
         driver.findElement(By.name("password")).sendKeys(password);
         driver.findElement(By.cssSelector("[value='Login']")).click();
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/center/table/tbody/tr[4]/td/span")));
-
         String actual = driver.findElement(By.className("error_message")).getText();
         Assert.assertEquals(actual, "Oops, error. Email and/or password don't match our records", "Сообщение об ошибке не совпадает");
     }
-
-
 
     @AfterMethod(alwaysRun = true)
     public void quit()
--- src/test/java/ZipCodeTest.java
+++ src/test/java/ZipCodeTest.java
@@ -1,4 +1,56 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import java.time.Duration;
+
 public class ZipCodeTest
 {
+    WebDriver driver;
+    WebDriverWait wait;
 
+    @BeforeMethod
+    public void Start()
+    {
+        driver = new ChromeDriver();
+        driver.get("https://sharelane.com/cgi-bin/register.py");
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void checkZipCode4Digits()
+    {
+        WebElement zip = driver.findElement(By.name("zip_code"));
+        zip.sendKeys("1234");
+        driver.findElement(By.cssSelector("[value='Continue']")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/center/table/tbody/tr[4]/td/span")));
+        String errorMessage;
+        errorMessage = driver.findElement(By.cssSelector("error_message")).getText();
+        Assert.assertEquals(errorMessage, "Oops, error on page. ZIP code should have 5 digits");
+    }
+
+    @Test
+    public void checkZipCode5Digits()
+    {
+        driver.findElement(By.name("zip_code")).sendKeys("12345");
+        driver.findElement(By.cssSelector("[value='Continue']")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("first_name")));
+        boolean elementOnPage;
+        elementOnPage = driver.findElement(By.xpath("/html/body/center/table/tbody/tr[1]/td/table/tbody/tr/td[2]/span")).isDisplayed();
+        Assert.assertTrue(elementOnPage);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void quit()
+    {
+        driver.quit();
+    }
 }
